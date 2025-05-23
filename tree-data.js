aadce7730f8f078c3cc2a2f610262e49730ad620
lexurkova <alex.surkova94@gmail.com>
2025-05-23 04:22:15 +0300

    Add files via upload

--- tree-data.js
+++ tree-data.js
@@ -1,57 +1,40 @@
+
 var chart_config = {
-  "chart": {
-    "container": "#tree-simple",
-    "nodeAlign": "TOP",
-    "connectors": {
-      "type": "step"
-    },
-    "node": {
-      "HTMLclass": "person-node"
-    }
+  chart: {
+    container: "#tree-simple",
+    nodeAlign: "TOP",
+    connectors: { type: "step" },
+    node: { HTMLclass: "person-node" }
   },
-  "nodeStructure": {
-    "text": {
-      "name": "Александра Суркова"
-    },
-    "image": "https://via.placeholder.com/80?text=AS",
-    "children": [
+  nodeStructure: {
+    text: { name: "Александра Суркова" },
+    image: "https://via.placeholder.com/80?text=AS",
+    children: [
       {
-        "text": {
-          "name": "Дарья Слабий"
-        },
-        "image": "https://via.placeholder.com/80?text=DS",
-        "children": [
+        text: { name: "Дарья Слабий" },
+        image: "https://via.placeholder.com/80?text=DS",
+        children: [
           {
-            "text": {
-              "name": "Людмила Климачкова"
-            },
-            "image": "https://via.placeholder.com/80?text=LK"
+            text: { name: "Людмила Климачкова" },
+            image: "https://via.placeholder.com/80?text=LK"
           },
           {
-            "text": {
-              "name": "Валентин Слабий"
-            },
-            "image": "https://via.placeholder.com/80?text=VS"
+            text: { name: "Валентин Слабий" },
+            image: "https://via.placeholder.com/80?text=VS"
           }
         ]
       },
       {
-        "text": {
-          "name": "Андрей Боляновский"
-        },
-        "image": "https://via.placeholder.com/80?text=AB",
-        "children": [
+        text: { name: "Андрей Боляновский" },
+        image: "https://via.placeholder.com/80?text=AB",
+        children: [
           {
-            "text": {
-              "name": "Галина Моцкус"
-            },
-            "image": "https://via.placeholder.com/80?text=GM"
+            text: { name: "Галина Моцкус" },
+            image: "https://via.placeholder.com/80?text=GM"
           },
           {
-            "text": {
-              "name": "Александр Боляновский"
-            },
-            "image": "https://via.placeholder.com/80?text=ALB"
+            text: { name: "Александр Боляновский" },
+            image: "https://via.placeholder.com/80?text=ALB"
           }
         ]
       }
